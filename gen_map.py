2d8e3ab19134475c5dd4aed46512f54f145de31a
Rahul <panwarrishi96@gmail.com>
2019-05-05 23:21:15 +0530

    Add files via upload
    RBM model
    Map generator

---
+++ gen_map.py
@@ -0,0 +1,152 @@
+import numpy as np
+import scipy
+import keras
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras.models import Model
+from keras.layers import Input
+from keras import backend as K
+from PIL import Image
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.models import load_model
+import scipy.misc
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+import h5py
+
+
+# Load the latest saved model (model_name.h5)
+model = load_model('model_name.h5')
+
+xtl=[]
+
+# path of the image
+path = "codes_GRBM/codes_GRBM/target_images/3.jpg"
+img = image.load_img(path)
+img = image.img_to_array(img)
+
+# CONSTANTS
+imagewidth  = 256
+imageheight = 256
+windowsize  = 32
+stride      = 2
+
+# iterations 
+iterations_w = (imagewidth - windowsize)/stride  +  1 
+iterations_h = (imageheight - windowsize)/stride  +  1 
+ 
+
+####### SCORES ARRAY INSTEAD OF THIS 
+score = np.random.uniform(low=0, high=1, size=(iterations_h*iterations_w,))
+
+# scores' data
+lowest_score  = np.amin(score)
+highest_score = np.amax(score)
+step_size     = (highest_score - lowest_score)/5
+
+# image for heatmap 
+newim = np.zeros((3,256,256),dtype='float32')
+
+# intensity control factor (to change contrast)
+icf = 20
+
+# increment counter
+l = 0
+
+def rgb(value):
+    # minimum, maximum = lowest_score, highest_score
+    ratio = 2 * (value- lowest_score) / (highest_score - lowest_score)
+    b = int(max(0, 255*(1 - ratio)))
+    r = int(max(0, 255*(ratio - 1)))
+    g = 255 - b - r
+    return r, g, b
+
+
+# define standard constant colors
+colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
+
+
+# set tolerance
+EPSILON = (highest_score - lowest_score)/5
+
+
+# processing image data
+def rgb( val):
+    fi = float(val- lowest_score) / float(highest_score- lowest_score) * (len(colors)-1)
+    i = int(fi)
+    f = fi - i
+    if f < EPSILON:
+        return colors[i]
+    else:
+        (r1, g1, b1), (r2, g2, b2) = colors[i], colors[i+1]
+        return int(r1 + f*(r2-r1)), int(g1 + f*(g2-g1)), int(b1 + f*(b2-b1))
+
+
+
+""" 
+		Color coding scheme
+		Pixel intensity is set according to the memorability score of a window stride
+
+"""
+for i in range(iterations_w):
+	for j in range(iterations_h):
+		# (r,g,b)
+		a = rgb(score[l])
+
+		# defining intervals for color coding
+		if(score[l]>=lowest_score and score[l]<lowest_score+step_size):
+			newim[0][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = a[0]
+			newim[1][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = a[1]
+			newim[2][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = a[2]#(score[l] - lowest_score)*icf
+
+		elif(score[l]>=lowest_score+step_size and score[l]<lowest_score+2*step_size):
+			newim[0][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 0
+			newim[1][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = (score[l] - lowest_score)*icf
+			newim[2][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 1
+	
+		elif(score[l]>=lowest_score+2*step_size and score[l]<lowest_score+3*step_size):
+			newim[0][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 0
+			newim[1][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 1
+			newim[2][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = (highest_score - score[l])*icf
+
+		elif(score[l]>=lowest_score+3*step_size and score[l]<lowest_score+4*step_size):
+			newim[0][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = (score[l] - lowest_score)*icf
+			newim[1][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 1
+			newim[2][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 0
+	
+		else:
+			# if(score[l]>=lowest_score+4*step_size and score[l]<=lowest_score+5*step_size)
+			newim[0][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 1
+			newim[1][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = (highest_score - score[l])*icf
+			newim[2][(j*stride):(j*stride)+windowsize,(i*stride):(i*stride)+windowsize] = 0
+		l+=1	
+
+
+# changing dimension to display image 
+newim = np.rollaxis(newim,0,3)
+
+
+"""
+ 		FOR SAVING HEATMAP NEW IMAGE
+	    'outfile.jpg' is the name of map image
+
+"""
+scipy.misc.imsave('outfile.jpg', newim)
+
+# Load the image and the image map
+img_map = image.load_img('outfile.jpg')
+orig_image = image.load_img(path)
+
+# display the image
+plt.imshow(Image.blend(orig_image, img_map, alpha=.9))
+plt.show()
---
+++ rbm_custom.py
@@ -0,0 +1,87 @@
+"""
+		Regression using RBM 
+"""
+
+import csv
+import numpy as np
+import pickle
+import scipy
+import keras
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras.models import Model
+from keras.layers import Input
+from keras import backend as K
+from PIL import Image
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras import backend as K
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+import itertools as it
+
+
+# training set nomenclature
+def name_image(name=1):
+	if len(name)>8:
+		return name
+	else:	
+		for i in range(8-len(name)):
+			name = "0"+name
+		return name
+
+
+def gen(batch_size=1, flag='train'):
+	if flag=='train':
+		start = 1
+		end   = 3
+	else:
+		start = 3
+		end   = 4																		
+	x_train = np.zeros((batch_size, 256*256), dtype='float32')
+	y_train = np.zeros((batch_size, 1), dtype='float32')
+	while True:
+		for i in range(start, end):
+			for j in range(batch_size):
+				with open("scores.csv","rb") as f:
+					reader = csv.reader(f)
+					y_train[j,:] = float(list(reader)[j][0])
+				path = "codes_GRBM/codes_GRBM/target_images/"+str(j)+".jpg"
+				img = image.load_img(path,grayscale=True)
+				img = image.img_to_array(img)
+				img = preprocess_input(img, mode='tf')
+				x_train[j,:] = img.reshape(256*256)
+			yield x_train, y_train
+
+def baseline_model():
+	my_model = Sequential()
+	my_model.add(Dense(4,input_dim=256*256, activation='relu'))
+	my_model.add(Dense(1, activation='sigmoid'))
+	my_model.summary()
+	# Compile model
+	my_model.compile(loss='mean_squared_error', optimizer='sgd')
+	return my_model
+
+model = baseline_model()
+
+batch_size = 4
+
+model.fit_generator( gen(batch_size=batch_size),
+					 steps_per_epoch = 2000/batch_size,
+					 # steps_per_epoch = 10,
+					 epochs = 1,
+					 callbacks = None,
+					 validation_data = gen(batch_size=batch_size, flag='val'),
+					 validation_steps = 222/batch_size
+					 # validation_steps = 2
+					 )
+
+model.save('mdh.h5')
